d02cd1855341725b7bb91af16bc4b22b2458957b
HARDIKPAL7787 <96346837+HARDIKPAL7787@users.noreply.github.com>
2024-01-22 17:59:26 +0530

    Add files via upload

---
+++ R basketball project/analystics of basketball-4.r
@@ -0,0 +1,24 @@
+#Salary 
+myplot(Salary)
+myplot(Salary/Games)
+myplot(Salary/FieldGoals)
+
+# In games metrics 
+myplot(MinutesPlayed)
+myplot(Points)
+
+#in games metrics normalized
+myplot(FieldGoals/Games)
+myplot(FieldGoals/FieldGoalAttempts)
+myplot(FieldGoalAttempts/Games)
+myplot(Points/Games)
+
+#interesting observation 
+myplot(MinutesPlayed/Games)
+myplot(Games)
+
+#time is valuable
+myplot(FieldGoals/MinutesPlayed)
+
+#player style
+myplot(Points/FieldGoals)
---
+++ R basketball project/function in matrix -2.r
@@ -0,0 +1,18 @@
+Games
+rownames(Games)
+colnames(Games)
+
+Games["LeBronJames","2012"]
+
+
+FieldGoals
+
+round(FieldGoals/Games,1)
+
+round(MinutesPlayed/Games,1)
+
+
+FieldGoalAttempts
+round(FieldGoalAttempts/Games)
+
+round( FieldGoalAttempts/FieldGoals)%%100
